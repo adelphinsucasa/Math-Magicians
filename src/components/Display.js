7e91805dca8c074b33d9657964357a05cde72674
josedelphin <dalejandro.005@gmail.com>
2023-02-21 18:51:40 -0400

    Add Buttons

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="display">0</div>
+    );
+  }
+}
