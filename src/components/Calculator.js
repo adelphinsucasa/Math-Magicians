26d2ad195fd5492715d2f64bed121f0121a05632
josedelphin <dalejandro.005@gmail.com>
2023-02-23 19:17:38 -0400

    Refactor from a class based component to a functional component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,58 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function Calculator() {
+  const [values, setValues] = useState({});
 
-  handleClick = (e) => {
-    this.setState((prevState) => calculate(prevState, e.target.innerText));
+  const handleClick = (e) => {
+    setValues((prevState) => calculate(prevState, e.target.innerText));
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="btnContainer">
-        <Display value={(total || next || '0')} />
-        <div className="pnlButtonsContainer">
-          <div className="pnlMainButtons">
-            <div className="pnlRow">
-              <Button name="AC" onclick={this.handleClick} />
-              <Button name="+/-" onclick={this.handleClick} />
-              <Button name="%" onclick={this.handleClick} />
-              <Button name="÷" addClass="orange" onclick={this.handleClick} />
-            </div>
-            <div className="pnlRow">
-              <Button name="7" onclick={this.handleClick} />
-              <Button name="8" onclick={this.handleClick} />
-              <Button name="9" onclick={this.handleClick} />
-              <Button name="x" addClass="orange" onclick={this.handleClick} />
-            </div>
-            <div className="pnlRow">
-              <Button name="4" onclick={this.handleClick} />
-              <Button name="5" onclick={this.handleClick} />
-              <Button name="6" onclick={this.handleClick} />
-              <Button name="-" addClass="orange" onclick={this.handleClick} />
-            </div>
-            <div className="pnlRow">
-              <Button name="1" onclick={this.handleClick} />
-              <Button name="2" onclick={this.handleClick} />
-              <Button name="3" onclick={this.handleClick} />
-              <Button name="+" addClass="orange" onclick={this.handleClick} />
-            </div>
-            <div className="pnlRow">
-              <Button name="0" addClass="doblesize" onclick={this.handleClick} />
-              <Button name="." onclick={this.handleClick} />
-              <Button name="=" addClass="orange" onclick={this.handleClick} />
-            </div>
+  return (
+    <div className="btnContainer">
+      <Display value={(values.total || values.next || '0')} />
+      <div className="pnlButtonsContainer">
+        <div className="pnlMainButtons">
+          <div className="pnlRow">
+            <Button name="AC" onclick={(e) => handleClick(e)} />
+            <Button name="+/-" onclick={(e) => handleClick(e)} />
+            <Button name="%" onclick={(e) => handleClick(e)} />
+            <Button name="÷" addClass="orange" onclick={(e) => handleClick(e)} />
+          </div>
+          <div className="pnlRow">
+            <Button name="7" onclick={(e) => handleClick(e)} />
+            <Button name="8" onclick={(e) => handleClick(e)} />
+            <Button name="9" onclick={(e) => handleClick(e)} />
+            <Button name="x" addClass="orange" onclick={(e) => handleClick(e)} />
+          </div>
+          <div className="pnlRow">
+            <Button name="4" onclick={(e) => handleClick(e)} />
+            <Button name="5" onclick={(e) => handleClick(e)} />
+            <Button name="6" onclick={(e) => handleClick(e)} />
+            <Button name="-" addClass="orange" onclick={(e) => handleClick(e)} />
+          </div>
+          <div className="pnlRow">
+            <Button name="1" onclick={(e) => handleClick(e)} />
+            <Button name="2" onclick={(e) => handleClick(e)} />
+            <Button name="3" onclick={(e) => handleClick(e)} />
+            <Button name="+" addClass="orange" onclick={(e) => handleClick(e)} />
+          </div>
+          <div className="pnlRow">
+            <Button name="0" addClass="doblesize" onclick={(e) => handleClick(e)} />
+            <Button name="." onclick={(e) => handleClick(e)} />
+            <Button name="=" addClass="orange" onclick={(e) => handleClick(e)} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
