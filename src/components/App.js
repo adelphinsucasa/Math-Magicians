f80853cb14736cf76459f712a3135d57e2ee5229
josedelphin <dalejandro.005@gmail.com>
2023-02-23 03:03:09 -0400

    Add EventListener

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Calculator from './Calculator';
+
+const App = () => (
+  <div className="Calculator">
+    <header>
+      <h1>Math Magicians by Jose Delphin</h1>
+    </header>
+    <Calculator />
+  </div>
+);
+
+export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,58 @@
+import React from 'react';
 import Display from './Display';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
-function Calculator() {
-  return (
-    <div className="Calculator">
-      <header>
-        <h1>Math Magicians by Jose Delphin</h1>
-      </header>
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    this.setState((prevState) => calculate(prevState, e.target.innerText));
+  };
+
+  render() {
+    const { total, next } = this.state;
+    return (
       <div className="btnContainer">
-        <Display />
+        <Display value={(total || next || '0')} />
         <div className="pnlButtonsContainer">
           <div className="pnlMainButtons">
             <div className="pnlRow">
-              <Button name="AC" />
-              <Button name="+/-" />
-              <Button name="%" />
+              <Button name="AC" onclick={this.handleClick} />
+              <Button name="+/-" onclick={this.handleClick} />
+              <Button name="%" onclick={this.handleClick} />
+              <Button name="÷" addClass="orange" onclick={this.handleClick} />
             </div>
             <div className="pnlRow">
-              <Button name="7" />
-              <Button name="8" />
-              <Button name="9" />
+              <Button name="7" onclick={this.handleClick} />
+              <Button name="8" onclick={this.handleClick} />
+              <Button name="9" onclick={this.handleClick} />
+              <Button name="x" addClass="orange" onclick={this.handleClick} />
             </div>
             <div className="pnlRow">
-              <Button name="4" />
-              <Button name="5" />
-              <Button name="6" />
+              <Button name="4" onclick={this.handleClick} />
+              <Button name="5" onclick={this.handleClick} />
+              <Button name="6" onclick={this.handleClick} />
+              <Button name="-" addClass="orange" onclick={this.handleClick} />
             </div>
             <div className="pnlRow">
-              <Button name="1" />
-              <Button name="2" />
-              <Button name="3" />
+              <Button name="1" onclick={this.handleClick} />
+              <Button name="2" onclick={this.handleClick} />
+              <Button name="3" onclick={this.handleClick} />
+              <Button name="+" addClass="orange" onclick={this.handleClick} />
             </div>
             <div className="pnlRow">
-              <div className="btnCero">
-                <Button name="0" />
-              </div>
-              <Button name="." />
+              <Button name="0" addClass="doblesize" onclick={this.handleClick} />
+              <Button name="." onclick={this.handleClick} />
+              <Button name="=" addClass="orange" onclick={this.handleClick} />
             </div>
-          </div>
-          <div className="pnlRightButtons">
-            <Button name="÷" />
-            <Button name="*" />
-            <Button name="-" />
-            <Button name="+" />
-            <Button name="=" />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Calculator;
